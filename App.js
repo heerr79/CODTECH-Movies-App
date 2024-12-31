a00a9f7b6ed88b5b608d7b4eea752e8d29410376
heerr79 <heerrpanchal@gmail.com>
2024-12-31 17:17:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieApp from './components/MovieApp.js';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieApp />
+    </div>
+  );
+};
+
+export default App;
